36ea8b2a6f02ae5d31a08e92ae786dbbfc3de914
Franco Cedillo <franco.cedillo@gmail.com>
2014-10-28 11:37:09 -0500

    adds tests

--- test/tagCollection_spec.rb
+++ test/tagCollection_spec.rb
@@ -2,14 +2,28 @@ require_relative 'spec_helper'
 require 'tagCollection'
 
 describe TagCollection, "complementarity" do
-  it "tagged items are complementary to untagged items" do
-    info = YAML.load(File.read('test/info.yml'))
-    list = info['list']
+  
+  info = YAML.load(File.read('test/info.yml'))
+  list = info['list']
 
-    itemLoader = ItemLoader.new(list)
-    taggedItems = itemLoader.getTaggedItems
-    untaggedItems = itemLoader.getUntaggedItems
+  itemLoader = ItemLoader.new(list)
+  taggedItems = itemLoader.getTaggedItems
+  untaggedItems = itemLoader.getUntaggedItems
 
+  it "list is not empty" do
+    expect(list.length).not_to eq(0)
+  end
+
+  it "taggedItems is not empty" do
+    expect(taggedItems.length).not_to eq(0)
+  end
+
+  it "untaggedItems is not empty" do
+    expect(taggedItems.length).not_to eq(0)
+  end
+  
+  it "taggedItems is complementary to untaggedItems" do
     expect(list.length).to eq(taggedItems.length + untaggedItems.length)
   end
+
 end
